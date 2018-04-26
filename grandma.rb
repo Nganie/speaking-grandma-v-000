cce1f748163cae1538112935da9f892f74f92899
Nganie <ngan.tp.quan@gmail.com>
2018-04-26 23:50:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,10 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase.upcase 
+elsif phrase.upcase
   return "NO, NOT SINCE 1938!"
-else "HUH?! SPEAK UP, SONNY!"
+else 
+  return "HUH?! SPEAK UP, SONNY!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
